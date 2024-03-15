fc12b288cb25adecf9a3d110aabbaec10d295ce9
Rahul <96195239+Mr-MRahul@users.noreply.github.com>
2024-03-15 15:26:20 +0530

    With Products,Orders Pages not completed Dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard'
+import Products from './pages/Products';
+import Orders from './pages/Orders';
+import Layout from './components/Layout';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Products />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
